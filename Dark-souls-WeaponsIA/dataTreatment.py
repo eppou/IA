0894850135f42722e660d34e10804248e1e79c74
Marcos V. Pretti <103339147+eppou@users.noreply.github.com>
2023-09-28 00:35:14 -0300

    update with view of the graph

--- Dark-souls-WeaponsIA/dataTreatment.py
+++ Dark-souls-WeaponsIA/dataTreatment.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 import random
+import matplotlib.pyplot as plt
+import networkx as nx
+
 
 def dataFrame_creator(df_og):
     df = df_og.copy()
@@ -14,3 +17,15 @@ def dataFrame_creator(df_og):
     df_physical = df.head(10).copy()
 
     return df_physical, df_lightning, df_fire, df_magic
+
+
+def imprimeGrafo(G):
+    # Desenhe o grafo
+    pos = nx.circular_layout(G)  # Layout para organizar os nós
+    nx.draw(G, pos, with_labels=True, node_color='lightblue', font_weight='bold')
+
+    # Exiba o grafo (opcional)
+    plt.show()
+
+    # Salve o grafo como imagem
+    plt.savefig("grafo.png")
