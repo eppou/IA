16bd58b54b31263a75d557393747257455b43abe
eppou <marcosvinicius.pretti@uel.br>
2023-09-29 19:11:56 -0300

    fix a*

--- Dark-souls-WeaponsIA/Heuristic.py
+++ Dark-souls-WeaponsIA/Heuristic.py
@@ -11,7 +11,6 @@ boss_df = pd.read_csv("bosses_csv.csv")
 weapond_df[['Physical damage','Magical damage','Fire damage','Lightning damage','Dark damage']] = weapond_df['Damage'].str.split('/',expand=True)
 colunas_para_converter = ['Physical damage', 'Magical damage', 'Fire damage', 'Lightning damage', 'Dark damage']
 
-# Itere pelas colunas e converta os valores em números
 for coluna in colunas_para_converter:
     weapond_df[coluna] = pd.to_numeric(weapond_df[coluna], errors='coerce')
 
@@ -25,32 +24,31 @@ def Pontuacao(arma, chefe):
     veloz = chefe['Velocidade']
     peso_arma = arma['Weight']
 
-    pontuacao = int(dano_geral)
+    pontuacao = 635 - (int(dano_geral) + int(dano_magico_arma) + int(dano_fogo_arma) + int(dano_raio_arma))
 
     if fraqueza_chefe == 'magic':
-        pontuacao += int(dano_magico_arma * 5)
+        pontuacao -= int(dano_magico_arma * 2)
     elif fraqueza_chefe == 'fire':
-        pontuacao += int(dano_fogo_arma * 5)
+        pontuacao -= int(dano_fogo_arma * 2)
     elif fraqueza_chefe == 'light':
-        pontuacao += int(dano_raio_arma * 5)
+        pontuacao -= int(dano_raio_arma * 2)
 
     if resistencia_chefe == 'magic':
-        pontuacao += int(dano_magico_arma * 0.25)
+        pontuacao += int(dano_magico_arma * 1)
     elif resistencia_chefe == 'fire':
-        pontuacao += int(dano_fogo_arma * 0.25)
+        pontuacao += int(dano_fogo_arma * 1)
     elif resistencia_chefe == 'light':
-        pontuacao += int(dano_raio_arma * 0.25)
+        pontuacao += int(dano_raio_arma * 1)
 
     if veloz == 1:
-        pontuacao -= int(peso_arma * 8)
+        pontuacao += int(peso_arma * 5)
     elif veloz == 0:
-        pontuacao -= int(peso_arma * 0.5)
+        pontuacao += int(peso_arma * 1.5)
 
     return pontuacao
 
 
 def heuristic(weapon, boss):
-    # Calcule uma estimativa do custo usando a pontuação da arma
     weapon_score = G[boss][weapon]['weight']
     return weapon_score
 
@@ -62,15 +60,19 @@ def astar_search(graph, start, goal, heuristic, real_cost):
     came_from = {}
     g_score = {node: float('inf') for node in graph}
     g_score[start] = 0
-    
+    return_cost = 0
+
     while open_list:
         current, current_cost = min(open_list, key=lambda x: x[1])
         open_list.remove((current, current_cost))
         
         if current == goal:
+            print("Chegou no objetivo")
             path = [current]
+            return_cost += g_score[current]
             while current in came_from:
                 current = came_from[current]
+                return_cost += g_score[current]
                 path.append(current)
             path.reverse()
             return path, current_cost
@@ -78,29 +80,32 @@ def astar_search(graph, start, goal, heuristic, real_cost):
         for neighbor in graph.neighbors(current):
             custo_total = real_cost(current, neighbor)
             tentative_g_score = g_score[current] + custo_total
+
             if tentative_g_score < g_score[neighbor]:
                 came_from[neighbor] = current
                 g_score[neighbor] = tentative_g_score
                 f_score = g_score[neighbor] + heuristic(neighbor, current)
                 open_list.append((neighbor, f_score))
     
-    path = [current]
-    while current in came_from:
-        current = came_from[current]
-        path.append(current)
-        path.reverse()
-    return path, custo_total
+    # Se não encontrou o objetivo, retorne o nó com a menor pontuação
+    min_node, min_score = min([(node, score) for node, score in g_score.items() if node != start], key=lambda x: x[1], default=(None, None))
+    path = [min_node]
+    while min_node in came_from:
+        return_cost += g_score[min_node]
+        min_node = came_from[min_node]
+        path.append(min_node)
+    path.reverse()
+    return path, return_cost
 
-def goal_weapon(boss, weapons_data):
-    # Calcule a arma ideal para derrotar o chefe
-    weapons = weapons_data.keys()
+def goal_weapon(boss, weapons_df):
     best_weapon = None
     best_score = -1
-    for weapon in weapons:
-        score = Pontuacao(weapons_data[weapon], boss)
+    for index,weapon in weapons_df.iterrows():
+        score = Pontuacao(weapon, boss)
         if score > best_score:
             best_weapon = weapon
             best_score = score
+    
     return best_weapon
 
 
@@ -111,31 +116,32 @@ G = nx.DiGraph()
 G.add_node(boss['Nomes'])
 
 
-# Crie um dicionário para mapear armas para seus dados
 weapons_data = {row['Name']: row for index, row in weapond_df.iterrows()}
 
-# Calcule as pontuações e adicione as arestas do boss para as armas
 df_physical, df_lightning, df_fire, df_magic = dt.dataFrame_creator(weapond_df)
 
 
-# Calcule as pontuações e adicione as arestas do chefe para armas aleatórias de cada DataFrame
 for df_name, df in [("Physical", df_physical), ("Lightning", df_lightning), ("Fire", df_fire), ("Magic", df_magic)]:
-    random_weapon = df.iloc[random.randint(0,9)]  # Escolha uma arma aleatória do DataFrame
+    numero_aleatorio = random.randint(0,len(df) - 1)
+    random_weapon = df.iloc[numero_aleatorio]  
     score = Pontuacao(random_weapon, boss)
     G.add_edge(boss['Nomes'], random_weapon['Name'], weight=score)
 
     for index, weapon_row in df.iterrows():
         if weapon_row['Name'] != random_weapon['Name']:
-            # Calcule a pontuação para esta conexão (você pode ajustar a lógica conforme necessário)
             score = Pontuacao(weapon_row, boss)
             G.add_edge(random_weapon['Name'], weapon_row['Name'], weight=score)
 
-
-goal = goal_weapon(boss, weapons_data)
+best_weapon = goal_weapon(boss, weapond_df)
+weapons_data[best_weapon['Name']] = best_weapon
+goal = best_weapon['Name']
+print(f"a melhor arma possivel seria {best_weapon['Name']} com os dados {best_weapon}")
 
 best_weapon_path, custo = astar_search(G, boss['Nomes'], goal , heuristic, real_cost)
 if best_weapon_path:
-    best_weapon = best_weapon_path[-1]  # A última arma no caminho é a melhor
-    print(f"A melhor arma para derrotar o chefe {boss['Nomes']} é: {best_weapon} o custo foi {(int)(custo)} caminho foi {best_weapon_path}")
+    best_weapon = best_weapon_path[-1]  
+    print(f"A melhor arma para derrotar o chefe {boss['Nomes']} é: {best_weapon} dados da arma")
+    print(weapons_data[best_weapon])
+    print(f"o custo foi {custo} o caminho foi {best_weapon_path} ")
 else:
     print("Nenhuma arma adequada encontrada.")
--- Dark-souls-WeaponsIA/dataTreatment.py
+++ Dark-souls-WeaponsIA/dataTreatment.py
@@ -4,17 +4,38 @@ import random
 import matplotlib.pyplot as plt
 import networkx as nx
 
+def aleatorize(df):
+    if len(df) >= 10:
+        aleatorize = random.sample(range(len(df)), 10)
+        new_df = df.iloc[aleatorize].copy()
+    else:
+        aleatorize = random.sample(range(len(df)), len(df))
+        new_df = df.iloc[aleatorize].copy()
+    return new_df
 
 def dataFrame_creator(df_og):
     df = df_og.copy()
-    df.sort_values(by=['Magical damage'], inplace=True)
-    df_magic = df.head(10).copy()
-    df.sort_values(by=['Fire damage'], inplace=True)
-    df_fire = df.head(10).copy()
-    df.sort_values(by=['Lightning damage'], inplace=True)
-    df_lightning = df.head(10).copy()
-    df.sort_values(by=['Physical damage'], inplace=True)
-    df_physical = df.head(10).copy()
+
+    df_magic = df[df['Magical damage'] > 0].copy()
+    df_magic = aleatorize(df_magic)
+    df_magic = df_magic.sort_values(by=['Magical damage'], ascending=False)  # Atualize o DataFrame com a classificação
+    df_magic = df_magic.reset_index(drop=True)
+
+    df_fire = df[df['Fire damage'] > 0].copy()
+    df_fire = aleatorize(df_fire)
+    df_fire = df_fire.sort_values(by=['Fire damage'], ascending=False)  # Atualize o DataFrame com a classificação
+    df_fire = df_fire.head(10).reset_index(drop=True)
+
+    df_lightning = df[df['Lightning damage'] > 0].copy()
+    df_lightning = aleatorize(df_lightning)
+    df_lightning = df_lightning.sort_values(by=['Lightning damage'], ascending=False)  # Atualize o DataFrame com a classificação
+    df_lightning = df_lightning.head(10).reset_index(drop=True)
+
+    df_physical = df[df['Physical damage'] > 0].copy()
+    df_physical = aleatorize(df_physical)
+    df_physical = df_physical.sort_values(by=['Physical damage'], ascending=False)  # Atualize o DataFrame com a classificação
+    df_physical = df_physical.reset_index(drop=True)
+
 
     return df_physical, df_lightning, df_fire, df_magic
 
